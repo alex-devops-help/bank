b71efc73b9bdf89f55c9a6b5e8ffc2c74aca9af9
Alexander <alex@5171967-ty32184>
2025-07-05 22:46:39 +0300

    mimikube installer

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@ docker-compose down --remove-orphans $Service;
 
 docker rmi -f $(docker images | grep bank | awk '{print $3}');
 docker system prune -f;
+docker volume prune -f;
 
 docker compose build --no-cache $Service;
 docker-compose up --force-recreate -d $Service;
---
+++ utils/recreate_minikube.sh
@@ -0,0 +1,48 @@
+set -x;
+
+Version=v1.32.6;
+#Version=v1.31.10;
+
+start_mk()
+{
+
+	minikube start \
+	  --kubernetes-version=$Version \
+	  --cni=flannel \
+	  --apiserver-ips=0.0.0.0 \
+	  --apiserver-port=8444 \
+	  --driver=docker \
+	  --extra-config=kubeadm.ignore-preflight-errors=Swap \
+	  --feature-gates=NodeSwap=true \
+	  --extra-config=kubelet.fail-swap-on=false;
+#	  --extra-config=kubelet.memory-swap.swap-behavior=LimitedSwap \
+
+
+}
+
+enable_addons()
+{
+	local Addons=$@;
+	for A in $Addons; do
+		minikube addons enable $A;
+	done;
+	minikube addons list
+}	
+
+minikube stop; minikube delete; start_mk;	
+
+sleep 10s;
+
+
+enable_addons csi-hostpath-driver dashboard headlamp logviewer metrics-server portainer registry ingress storage-provisioner-rancher volumesnapshots storage-provisioner-rancher yakd;
+# volcano # - batch scheduler
+
+minikube kubectl -- create token headlamp --duration 24h -n headlamp;
+
+minikube node add;
+
+minikube profile list;
+
+minikube kubectl -- get all -A;
+
+minikube kubectl config view;
