26d52244f32994904ba0a7313df6da981e314572
Alexander <alex@5171967-ty32184>
2025-07-10 11:40:36 +0300

    Removed unused code in recreate_minikube.sh

--- utils/recreate_minikube.sh
+++ utils/recreate_minikube.sh
@@ -7,17 +7,10 @@ start_mk()
 {
 
 	minikube start \
-	  --kubernetes-version=$Version \
-	  --cni=flannel \
-	  --apiserver-ips=0.0.0.0 \
-	  --apiserver-port=8444 \
-	  --driver=docker \
-	  --extra-config=kubeadm.ignore-preflight-errors=Swap \
-	  --feature-gates=NodeSwap=true \
-	  --extra-config=kubelet.fail-swap-on=false;
+	  --kubernetes-version=$Version --driver=docker \
+	  --cni=flannel --apiserver-ips=0.0.0.0 --apiserver-port=8444 \
+	  --extra-config=kubeadm.ignore-preflight-errors=Swap  --feature-gates=NodeSwap=true  --extra-config=kubelet.fail-swap-on=false;
 #	  --extra-config=kubelet.memory-swap.swap-behavior=LimitedSwap \
-
-
 }
 
 enable_addons()
@@ -26,23 +19,15 @@ enable_addons()
 	for A in $Addons; do
 		minikube addons enable $A;
 	done;
-	minikube addons list
+	minikube addons list;
 }	
 
-minikube stop; minikube delete; start_mk;	
-
-sleep 10s;
-
+minikube stop; minikube delete; start_mk; sleep 10s;
 
 enable_addons csi-hostpath-driver dashboard headlamp logviewer metrics-server portainer registry ingress storage-provisioner-rancher volumesnapshots storage-provisioner-rancher yakd;
-# volcano # - batch scheduler
-
-minikube kubectl -- create token headlamp --duration 24h -n headlamp;
+# volcano # batch scheduler
 
 minikube node add;
 
-minikube profile list;
-
-minikube kubectl -- get all -A;
-
-minikube kubectl config view;
+minikube kubectl -- create token headlamp --duration 24h -n headlamp;
+minikube profile list; minikube kubectl -- get all -A; minikube kubectl config view;
