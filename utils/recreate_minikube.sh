ac86f2418535c7fc2a5aeee0acf284e2a2b0f510
Alexander <alex@5171967-ty32184>
2025-07-13 08:36:23 +0300

    Some fixes

--- utils/recreate_minikube.sh
+++ utils/recreate_minikube.sh
@@ -46,4 +46,4 @@ minikube node add;
 minikube kubectl -- create token headlamp --duration 24h -n headlamp;
 minikube profile list; minikube kubectl -- get all -A; minikube kubectl config view;
 chmod 750 -R ~/.minikube;
-chmod 660 ~/.kube/config;
+chmod 666 ~/.kube/config;
