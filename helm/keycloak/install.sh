ec1fd02845a0167f81bfb9e47338371834a572af
Alexander <alex@5171967-ty32184>
2025-07-13 09:00:30 +0300

    Fixes to allow non root users to use scripts

--- helm/keycloak/install.sh
+++ helm/keycloak/install.sh
@@ -7,9 +7,3 @@ fi;
 ( kubectl get -n keycloak keycloak bank || ( kubectl create ns keycloak; olm/install.sh v0.32.0; echo "Result: $?"; sleep 35s ) ) && \
 kubectl apply -f keycloak-operator.yml && sleep 35s && ./recreate.sh;
 
-#kubectl apply -f bank-instance.yml && sleep 75s && \
-#./status.sh && \
-#/project/utils/load_json_clients.sh;
-
-#./password.sh;
-#bash -lc "kubectl -n keycloak logs pod/bank-0 -f";
--- helm/keycloak/recreate.sh
+++ helm/keycloak/recreate.sh
@@ -4,7 +4,7 @@ kubectl delete -f bank-instance.yml && sleep 3s;
 
 kubectl apply -f bank-instance.yml && sleep 75s && \
 ./status.sh && \
-/project/utils/load_json_clients.sh;
+~/project/utils/load_json_clients.sh;
 
 #./password.sh;
 #bash -lc "kubectl -n keycloak logs pod/bank-0 -f";
