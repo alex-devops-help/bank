53e227c4c7a7604d93acb738f9656985a205c095
Alexander Prokopyev <devops@aulix.com>
2025-07-15 17:10:31 +0300

    Jenkins agent image

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,10 +3,11 @@ set -x;
 KubeRegistryHost=registry.kube-system.svc.cluster.local;
 RegistryImage="registry.kube-system.svc.cluster.local:30050/jenkins:custom-v1";
 
+eval $(minikube docker-env);
 docker build . -t jenkins:custom-v1;
+#minikube image build . -t jenkins:custom-v1 --all=true; # --all for multinode minikube
 
-docker tag 	jenkins:custom-v1 	$RegistryImage;
-docker push 	$RegistryImage;
-
-curl http://minikube:30050/v2/_catalog;
-curl http://minikube:30050/v2/jenkins/tags/list;
+#docker tag 	jenkins:custom-v1 	$RegistryImage;
+#docker push 	$RegistryImage;
+#curl http://minikube:30050/v2/_catalog;
+#curl http://minikube:30050/v2/jenkins/tags/list;
